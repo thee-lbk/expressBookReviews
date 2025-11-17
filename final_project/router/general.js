d95f9527bb89c981c8b809ed375bd5585cfba6ad
theerachai.c <theerachai.c@kasikornline.com>
2025-11-17 17:52:56 +0000

    Task 5

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -61,8 +61,12 @@ public_users.get('/title/:title',function (req, res) {
 
 //  Get book review
 public_users.get('/review/:isbn',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  const isbn = req.params.isbn;
+  if (books[isbn]) {
+    return res.json(books[isbn].reviews);
+  } else {
+    return res.status(404).json({message: "Book not found"});
+  }
 });
 
 module.exports.general = public_users;
