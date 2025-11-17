95e1a97ed8d4da8cf96d9926cf3934e32825ef13
theerachai.c <theerachai.c@kasikornline.com>
2025-11-17 17:33:31 +0000

    modified:   final_project/router/general.js

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -17,8 +17,12 @@ public_users.get('/',function (req, res) {
 
 // Get book details based on ISBN
 public_users.get('/isbn/:isbn',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  const isbn = req.params.isbn;
+  if (books[isbn]) {
+    return res.json(books[isbn]);
+  } else {
+    return res.status(404).json({message: "Book not found"});
+  }
  });
   
 // Get book details based on author
