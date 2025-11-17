64cdfec062d995658ff447a6cb6fe699a67c1d67
theerachai.c <theerachai.c@kasikornline.com>
2025-11-17 18:22:12 +0000

    Task 10

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -22,7 +22,12 @@ public_users.post("/register", (req,res) => {
 
 // Get the book list available in the shop
 public_users.get('/',function (req, res) {
-  res.send(JSON.stringify(books,null,4));
+  let myPromise = new Promise((resolve,reject) => {
+      resolve(books);
+  })
+  myPromise.then((successMessage) => {
+    res.send(JSON.stringify(successMessage, null, 4));
+  })
 });
 
 // Get book details based on ISBN
